2a56efe19a54c6b98f150ac74e0f1fbae135d8d0
Robert Randiak <randiak@protonmail.com>
2025-03-26 16:54:38 +0100

    feat: use new version of solrify

--- kramerius/client/search.py
+++ kramerius/client/search.py
@@ -1,12 +1,14 @@
-from solrify import Q, SolrClient, SolrConfig
+from solrify import F, SolrClient, SolrConfig
 
 from ..custom_types import KrameriusField, Pid
 from ..schemas import KrameriusDocument
 
 
 class SearchClient(SolrClient[KrameriusDocument]):
+    document_type = KrameriusDocument
+
     def __init__(self, config: SolrConfig):
         super().__init__(config)
 
     def get_document(self, pid: Pid) -> KrameriusDocument | None:
-        return super().get(Q(KrameriusField.Pid, pid))
+        return super().get(F(KrameriusField.Pid, pid))
