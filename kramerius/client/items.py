3c63763a50b26bc0c961fcf6879fdf65d64e6a4a
Barbora Jurickova <barbora.jurickova@mzk.cz>
2025-08-07 15:16:49 +0200

    feat: add get document children method

--- kramerius/client/items.py
+++ kramerius/client/items.py
@@ -1,3 +1,5 @@
+import requests
+
 from .base import KrameriusBaseClient
 from lxml import etree
 
@@ -20,3 +22,10 @@ class ItemsClient:
             f"items/{pid}/image",
             data_type="image/jpeg",
         ).content
+
+    def get_children(self, pid: str) -> list[dict[str, str]]:
+        response: requests.Response = self._client.client_request_response(
+            "GET",
+            f"items/{pid}/info/structure",
+        )
+        return response.json()["children"]["own"]
