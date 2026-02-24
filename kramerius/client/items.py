2a18682f690e72ad9275d48ea7c130fbecc1efbb
Barbora Jurickova <barbora.jurickova@mzk.cz>
2026-02-24 17:20:05 +0100

    feat: add get doxument foxml full, get imageserver full image location

--- kramerius/client/items.py
+++ kramerius/client/items.py
@@ -1,8 +1,9 @@
+from requests import HTTPError
+
 from kramerius.definitions.akubra import Xml
 
 from .base import KrameriusBaseClient, response_to_bytes, response_to_xml
 import requests
-
 
 
 class ItemsClient:
@@ -39,3 +40,23 @@ class ItemsClient:
             f"api/client/v7.0/items/{pid}/info/structure",
         )
         return response.json()["model"]
+
+    def get_foxml_full(self, pid: str) -> Xml:
+        response: requests.Response = self._client.request(
+            "GET",
+            f"api/admin/v7.0/items/{pid}/info/structure",
+            # data_type="application/xml",
+        )
+        if response.status_code != 200:
+            raise HTTPError(f"{response.status_code}: {response.text}")
+        return response_to_xml(response)
+
+    def get_imageserver_location_full(self, pid: str) -> str:
+        ds_id = "IMG_FULL"
+        response: requests.Response = self._client.request(
+            "GET",
+            f"api/admin/v7.0/repository/getDatastreamMetadata?pid={pid}&dsId={ds_id}",
+        )
+        if response.status_code != 200:
+            raise HTTPError(f"{response.status_code}: {response.text}")
+        return response.json()["location"]
