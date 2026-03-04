d4332a93e53f022f97baa50e94245a1bfcd258ac
Barbora Jurickova <barbora.jurickova@mzk.cz>
2026-03-04 15:31:44 +0100

    feat: get OCR text

--- kramerius/client/items.py
+++ kramerius/client/items.py
@@ -41,6 +41,42 @@ class ItemsClient:
         )
         return response.json()["model"]
 
+    def get_ocr_text(self, pid: str) -> str:
+        """
+        Retrieves OCR text as string, no editing from source.
+        :param pid: uuid
+        :return: string of OCR text
+        """
+        response: requests.Response = self._client.request(
+            "GET",
+            f"api/admin/v7.0/items/{pid}/ocr/text",
+            data_type="text/plain",
+        )
+        if response.status_code != 200:
+            raise HTTPError(f"{response.status_code}: {response.text}")
+        return response.text
+
+    def get_ocr_text_all_child_pages(self, pid: str) -> str:
+        """
+        Retrieves and concatenates OCR from children of :param pid: . Each page concatenated with a newline. If a child of :param pid: does not have ocr a warning message is printed but the process does not fail.
+        :param pid: uuid
+        :return: string of OCR texts
+        """
+        children = self.get_children(pid)
+        text: str = ""
+        for child in children:
+            try:
+                child_text = self.get_ocr_text(child['pid'])
+            except HTTPError:
+                print(f"Child {child['pid']} isn't a page or just doesn't have OCR")
+                continue
+            text += child_text + '\n'
+
+        if text == "":
+            print(f"FINAL: Document {pid} doesn't have pages or none of them had OCR")
+        return text
+
+
     def get_foxml_full(self, pid: str) -> Xml:
         """
         Retrieves full admin foxml of document
@@ -51,7 +87,6 @@ class ItemsClient:
         response: requests.Response = self._client.request(
             "GET",
             f"api/admin/v7.0/items/{pid}/info/structure",
-            # data_type="application/xml",
         )
         if response.status_code != 200:
             raise HTTPError(f"{response.status_code}: {response.text}")
