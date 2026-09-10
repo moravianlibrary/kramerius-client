b4c276302df665d9cffc0886eca85e61bad5537c
Barbora Jurickova <barbora.jurickova@mzk.cz>
2026-09-10 15:42:37 +0200

    feat: get_children through solr Search (with optional fields and sort)

--- kramerius/client/search.py
+++ kramerius/client/search.py
@@ -12,3 +12,14 @@ class SearchClient(SolrClient[KrameriusDocument]):
 
     def get_document(self, pid: Pid) -> KrameriusDocument | None:
         return super().get_one_or_none(F(KrameriusField.Pid, pid))
+
+    def get_children(self, pid: Pid, fields: list[str] | None = None, sort: KrameriusField | None = None) -> list[KrameriusDocument]:
+        """
+        Solr search for documents with pid as its parent.
+        :param pid: uuid of document
+        :param fields: list of fields to return, optional
+        :param sort: sort fields by, optional
+        :return: List od KrameriusDocument, sorted by field ascending
+        """
+        l: list[KrameriusDocument] = list(super().search(F(KrameriusField.ParentPid, pid), fl=fields))
+        return sorted(l,key=lambda doc: doc.model_dump(by_alias=True)[sort.value]) if sort else l
